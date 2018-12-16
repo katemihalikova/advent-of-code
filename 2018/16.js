60c046472ce599903ba21349967c2a9248c84c88
Kate Miháliková <kate@katemihalikova.cz>
2018-12-16 10:24:27 +0100

    Simplify 16.js

--- 2018/16.js
+++ 2018/16.js
@@ -8,33 +8,33 @@ function part1(samples) {
     return {before: [b0, b1, b2, b3], args: [a, b, c], after: [a0, a1, a2, a3]};
   });
 
-  let ops = {
-    addr: (reg, a, b, c) => reg[c] = reg[a] + reg[b],
-    addi: (reg, a, b, c) => reg[c] = reg[a] + b,
+  let possibleOps = [
+    (reg, a, b, c) => reg[c] = reg[a] + reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] + b,
 
-    mulr: (reg, a, b, c) => reg[c] = reg[a] * reg[b],
-    muli: (reg, a, b, c) => reg[c] = reg[a] * b,
+    (reg, a, b, c) => reg[c] = reg[a] * reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] * b,
 
-    banr: (reg, a, b, c) => reg[c] = reg[a] & reg[b],
-    bani: (reg, a, b, c) => reg[c] = reg[a] & b,
+    (reg, a, b, c) => reg[c] = reg[a] & reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] & b,
 
-    borr: (reg, a, b, c) => reg[c] = reg[a] | reg[b],
-    bori: (reg, a, b, c) => reg[c] = reg[a] | b,
+    (reg, a, b, c) => reg[c] = reg[a] | reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] | b,
 
-    setr: (reg, a, b, c) => reg[c] = reg[a],
-    seti: (reg, a, b, c) => reg[c] = a,
+    (reg, a, b, c) => reg[c] = reg[a],
+    (reg, a, b, c) => reg[c] = a,
     
-    gtir: (reg, a, b, c) => reg[c] = a > reg[b] ? 1 : 0,
-    gtri: (reg, a, b, c) => reg[c] = reg[a] > b ? 1 : 0,
-    gtrr: (reg, a, b, c) => reg[c] = reg[a] > reg[b] ? 1 : 0,
+    (reg, a, b, c) => reg[c] = a > reg[b] ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] > b ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] > reg[b] ? 1 : 0,
 
-    eqir: (reg, a, b, c) => reg[c] = a == reg[b] ? 1 : 0,
-    eqri: (reg, a, b, c) => reg[c] = reg[a] == b ? 1 : 0,
-    eqrr: (reg, a, b, c) => reg[c] = reg[a] == reg[b] ? 1 : 0,
-  };
+    (reg, a, b, c) => reg[c] = a == reg[b] ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] == b ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] == reg[b] ? 1 : 0,
+  ];
 
   return samples.filter(({args, before, after}) => {
-    let opsMatched = Object.values(ops).filter(op => {
+    let opsMatched = possibleOps.filter(op => {
       let reg = [...before];
       op(reg, ...args);
       return after.every((expected, i) => expected === reg[i]);
@@ -55,32 +55,32 @@ function part2(samples, input) {
 
   input = input.split("\n").map(id => id.split(" ").map(Number));
 
-  let ops = {
-    addr: (reg, a, b, c) => reg[c] = reg[a] + reg[b],
-    addi: (reg, a, b, c) => reg[c] = reg[a] + b,
+  let possibleOps = [
+    (reg, a, b, c) => reg[c] = reg[a] + reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] + b,
 
-    mulr: (reg, a, b, c) => reg[c] = reg[a] * reg[b],
-    muli: (reg, a, b, c) => reg[c] = reg[a] * b,
+    (reg, a, b, c) => reg[c] = reg[a] * reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] * b,
 
-    banr: (reg, a, b, c) => reg[c] = reg[a] & reg[b],
-    bani: (reg, a, b, c) => reg[c] = reg[a] & b,
+    (reg, a, b, c) => reg[c] = reg[a] & reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] & b,
 
-    borr: (reg, a, b, c) => reg[c] = reg[a] | reg[b],
-    bori: (reg, a, b, c) => reg[c] = reg[a] | b,
+    (reg, a, b, c) => reg[c] = reg[a] | reg[b],
+    (reg, a, b, c) => reg[c] = reg[a] | b,
 
-    setr: (reg, a, b, c) => reg[c] = reg[a],
-    seti: (reg, a, b, c) => reg[c] = a,
+    (reg, a, b, c) => reg[c] = reg[a],
+    (reg, a, b, c) => reg[c] = a,
     
-    gtir: (reg, a, b, c) => reg[c] = a > reg[b] ? 1 : 0,
-    gtri: (reg, a, b, c) => reg[c] = reg[a] > b ? 1 : 0,
-    gtrr: (reg, a, b, c) => reg[c] = reg[a] > reg[b] ? 1 : 0,
+    (reg, a, b, c) => reg[c] = a > reg[b] ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] > b ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] > reg[b] ? 1 : 0,
 
-    eqir: (reg, a, b, c) => reg[c] = a == reg[b] ? 1 : 0,
-    eqri: (reg, a, b, c) => reg[c] = reg[a] == b ? 1 : 0,
-    eqrr: (reg, a, b, c) => reg[c] = reg[a] == reg[b] ? 1 : 0,
-  };
+    (reg, a, b, c) => reg[c] = a == reg[b] ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] == b ? 1 : 0,
+    (reg, a, b, c) => reg[c] = reg[a] == reg[b] ? 1 : 0,
+  ];
 
-  let candidates = new Map(Object.values(ops).map(op => [op, new Set()]));
+  let candidates = new Map(possibleOps.map(op => [op, new Set()]));
 
   samples.forEach(({opCode, args, before, after}) => {
     candidates.forEach((possibleOpCodes, op) => {
@@ -93,7 +93,7 @@ function part2(samples, input) {
     });
   });
 
-  let opCodes = {};
+  let ops = {};
 
   while (true) {
     let knownOpCodes = new Set();
@@ -101,7 +101,7 @@ function part2(samples, input) {
     candidates.forEach((possibleOpCodes, op) => {
       if (possibleOpCodes.size === 1) {
         let opCode = possibleOpCodes.values().next().value;
-        opCodes[opCode] = op;
+        ops[opCode] = op;
         knownOpCodes.add(opCode);
         candidates.delete(op);
       }
@@ -115,7 +115,7 @@ function part2(samples, input) {
   }
 
   let reg = [0, 0, 0, 0];
-  input.forEach(([opCode, a, b, c]) => opCodes[opCode](reg, a, b, c));
+  input.forEach(([opCode, a, b, c]) => ops[opCode](reg, a, b, c));
   return reg[0];
 }
 
