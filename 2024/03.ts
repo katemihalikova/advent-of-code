cff1a21dbb6175299a19bebc1572916037683f92
Kate Miháliková <kate@katemihalikova.cz>
2024-12-03 18:53:13 +0100

    Add alternative 03.ts

--- 2024/03.ts
+++ 2024/03.ts
@@ -17,8 +17,14 @@ function part2(input: string): number {
     [0];
 }
 
+function part2_alt(input: string): number {
+  return part1(input.replaceAll(/don't\(\).*?(do\(\)|$)/gs, ""));
+}
+
 // == ASSERTS ==
 
 console.assert(part1(`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`) === 161);
 
 console.assert(part2(`xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`) === 48);
+
+console.assert(part2_alt(`xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`) === 48);
