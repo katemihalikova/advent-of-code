e1bb0eac989c54fcffada648c69b5955f5deaa0f
Kate Miháliková <kate@katemihalikova.cz>
2024-12-16 12:34:19 +0100

    Speed up 14.ts

--- 2024/14.ts
+++ 2024/14.ts
@@ -35,26 +35,47 @@ function part2(input: string, width: number, height: number): number {
       return {x, y, vx, vy};
     });
 
-  for (let second = 1;; second++) {
+  let maxRowCoherency = 0, rowCoherencySecond = 0;
+  let maxColumnCoherency = 0, columnCoherencySecond = 0;
+
+  for (let second = 1; second < width || second < height; second++) {
     for (let robot of robots) {
       robot.x = (robot.x + robot.vx + width) % width;
       robot.y = (robot.y + robot.vy + height) % height;
     }
 
-    let coherency = robots
-      .reduce((counts, {x,y}) => {
+    let rowCoherency = robots
+      .reduce<number[]>((counts, {x, y}) => {
         counts[y * width + x] = (counts[y * width + x] ?? 0) + 1;
         return counts;
-      }, Array(width * height))
-      .reduce((product, _, index, counts) => product * Math.max(counts.slice(index+1).findIndex(nextCount => !nextCount), 1), 1);
+      }, Array(width * height + 1))
+      .reduce((sum, count, index, counts) => sum + count * counts.slice(index).findIndex(nextCount => !nextCount), 0);
 
-    if (coherency > 10 ** width) return second;
+    let columnCoherency = robots
+      .reduce<number[]>((counts, {x, y}) => {
+        counts[x * height + y] = (counts[x * height + y] ?? 0) + 1;
+        return counts;
+      }, Array(width * height + 1))
+      .reduce((sum, count, index, counts) => sum + count * counts.slice(index).findIndex(nextCount => !nextCount), 0);
+
+    if (second <= width && rowCoherency > maxRowCoherency) {
+      maxRowCoherency = rowCoherency;
+      rowCoherencySecond = second;
+    }
+    if (second <= height && columnCoherency > maxColumnCoherency) {
+      maxColumnCoherency = columnCoherency;
+      columnCoherencySecond = second;
+    }
+  }
+
+  for (let second = rowCoherencySecond;; second += height) {
+    if (second % width === columnCoherencySecond) return second;
   }
 }
 
 // == ASSERTS ==
 
-console.assert(part1(`\
+let example = `\
 p=0,4 v=3,-3
 p=6,3 v=-1,-3
 p=10,3 v=-1,2
@@ -66,6 +87,9 @@ p=3,0 v=-1,-2
 p=9,3 v=2,3
 p=7,3 v=-1,2
 p=2,4 v=2,-3
-p=9,5 v=-3,-3`, 11, 7) === 12);
+p=9,5 v=-3,-3`;
+
+console.assert(part1(example, 11, 7) === 12);
 
 // No testing data provided for PART 2
+console.assert(part2 !== undefined);
