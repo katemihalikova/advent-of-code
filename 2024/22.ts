7683894d129a7b6f68e29f86214a71fdfb04ac6c
Kate Miháliková <kate@katemihalikova.cz>
2024-12-22 23:19:36 +0100

    Improve 22.ts

--- 2024/22.ts
+++ 2024/22.ts
@@ -7,6 +7,20 @@ function calculateNextSecretNumber(secretNumber: number): number {
   return secretNumber;
 }
 
+// Alternative implementations
+function calculateNextSecretNumber_alt1(secretNumber: number): number {
+  secretNumber = ((secretNumber << 6) ^ secretNumber) & 0xffffff;
+  secretNumber = ((secretNumber >> 5) ^ secretNumber) & 0xffffff;
+  secretNumber = ((secretNumber << 11) ^ secretNumber) & 0xffffff;
+  return secretNumber;
+}
+
+function calculateNextSecretNumber_alt2(secretNumber: number): number {
+  return 0x7ff & (secretNumber << 6)
+    ^ 0x7ffff & (secretNumber << 1 ^ secretNumber >> 5)
+    ^ 0xffffff & (secretNumber ^ secretNumber << 11 ^ secretNumber << 12 ^ secretNumber << 17);
+}
+
 // == PART 1 ==
 
 function part1(input: string): number {
@@ -58,6 +72,39 @@ function part2(input: string): number {
 }
 
 // == ASSERTS ==
+
+console.assert(calculateNextSecretNumber(123) === 15887950);
+console.assert(calculateNextSecretNumber(15887950) === 16495136);
+console.assert(calculateNextSecretNumber(16495136) === 527345);
+console.assert(calculateNextSecretNumber(527345) === 704524);
+console.assert(calculateNextSecretNumber(704524) === 1553684);
+console.assert(calculateNextSecretNumber(1553684) === 12683156);
+console.assert(calculateNextSecretNumber(12683156) === 11100544);
+console.assert(calculateNextSecretNumber(11100544) === 12249484);
+console.assert(calculateNextSecretNumber(12249484) === 7753432);
+console.assert(calculateNextSecretNumber(7753432) === 5908254);
+
+console.assert(calculateNextSecretNumber_alt1(123) === 15887950);
+console.assert(calculateNextSecretNumber_alt1(15887950) === 16495136);
+console.assert(calculateNextSecretNumber_alt1(16495136) === 527345);
+console.assert(calculateNextSecretNumber_alt1(527345) === 704524);
+console.assert(calculateNextSecretNumber_alt1(704524) === 1553684);
+console.assert(calculateNextSecretNumber_alt1(1553684) === 12683156);
+console.assert(calculateNextSecretNumber_alt1(12683156) === 11100544);
+console.assert(calculateNextSecretNumber_alt1(11100544) === 12249484);
+console.assert(calculateNextSecretNumber_alt1(12249484) === 7753432);
+console.assert(calculateNextSecretNumber_alt1(7753432) === 5908254);
+
+console.assert(calculateNextSecretNumber_alt2(123) === 15887950);
+console.assert(calculateNextSecretNumber_alt2(15887950) === 16495136);
+console.assert(calculateNextSecretNumber_alt2(16495136) === 527345);
+console.assert(calculateNextSecretNumber_alt2(527345) === 704524);
+console.assert(calculateNextSecretNumber_alt2(704524) === 1553684);
+console.assert(calculateNextSecretNumber_alt2(1553684) === 12683156);
+console.assert(calculateNextSecretNumber_alt2(12683156) === 11100544);
+console.assert(calculateNextSecretNumber_alt2(11100544) === 12249484);
+console.assert(calculateNextSecretNumber_alt2(12249484) === 7753432);
+console.assert(calculateNextSecretNumber_alt2(7753432) === 5908254);
 
 console.assert(part1(`\
 1
